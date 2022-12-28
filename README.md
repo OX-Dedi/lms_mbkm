8b3af08f6394461baa3ece634bd25d0d80c14724
OX-Dedi <96182960+OX-Dedi@users.noreply.github.com>
2022-12-28 15:42:23 +0700

    first commit

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Project LMS MBKM
 username : admin
 password : 04April2001-
 # Tel-U
+# Tel-U
--- lms_mbkm.sql
+++ lms_mbkm.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2022 at 03:11 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Dec 28, 2022 at 09:40 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,7 +36,7 @@ CREATE TABLE `activity` (
   `Resouce` varchar(50) NOT NULL,
   `Activity` varchar(100) NOT NULL,
   `Sum_Resource_Activity` int(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `activity`
@@ -59,7 +59,7 @@ CREATE TABLE `admin` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `Pembaruhan` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -79,7 +79,7 @@ CREATE TABLE `agenda` (
   `title` varchar(255) NOT NULL,
   `start_event` datetime NOT NULL,
   `end_event` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `agenda`
@@ -87,6 +87,32 @@ CREATE TABLE `agenda` (
 
 INSERT INTO `agenda` (`id`, `title`, `start_event`, `end_event`) VALUES
 (1, 'Meeting With Dedi', '2022-10-08 12:00:00', '2022-10-08 13:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `app_user`
+--
+
+CREATE TABLE `app_user` (
+  `id` bigint(10) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1,
+  `is_admin` int(1) NOT NULL DEFAULT 0,
+  `user_password` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `full_name` varchar(200) NOT NULL,
+  `registered_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `app_user`
+--
+
+INSERT INTO `app_user` (`id`, `status`, `is_admin`, `user_password`, `email`, `full_name`, `registered_on`) VALUES
+(1, 1, 1, '787f04d4569019803dfb6ab702ab7f26d86969da242628aed42d8a89775fbba34400a1520fd2dcc9a91370735261e93118f0aa58739581bf3bcbb50390d05cb4XGhVM2ALAff0L6jrJK1827w/hbCwTN9eMgl0WeMeQ84=', 'admin@mail.com', 'Dedi Rosadi', '2022-12-28 13:34:14'),
+(2, 1, 0, '1a2b223e73774315c7f5bb2dfe8aa3f75a8332c04a0fcae47d1745f8af5d48395a42b8702646a832f659b5e54010c5516ee8bb934bc57245a5c9fae69c20ccb29jgVaCri2uZ8jkVzpIooYoHu0NmuGHEbZPFqcHt7x08=', 'user1@mail.com', 'User 1', '2020-09-24 14:13:17'),
+(3, 1, 0, 'a1734e1caf3cc66b4786ead575c608cb08b433c8da225a4489309064d68c8824b0b791bba109fca51a806c7eea1c81bc3929d9c1cc579db65c288c74d0324e65I0clwoGrh/D3z5b6ovOEN0bXUQQSVp78G5prJLubqtg=', 'user2@mail.com', 'User 2', '2020-09-24 14:14:03'),
+(8, 1, 0, '8a54d8c89edc050c9a1c745afeaa7f92cad65b2ded4027045ce61fbbcddb2458c522071da70218354135e1a4d8857d40bb0a5a7699debbd397b5922b0dfcfc6aDz7FM/jqDQLUNYWW6ev9v1UiB4gluJ0Iam/3FLQE23Q=', 'tess@mail.com', 'tess', '2022-12-28 10:52:47');
 
 -- --------------------------------------------------------
 
@@ -105,15 +131,38 @@ CREATE TABLE `calendar` (
   `create_by` varchar(64) DEFAULT NULL,
   `modified_at` datetime DEFAULT NULL,
   `modified_by` varchar(64) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `calendar`
 --
 
 INSERT INTO `calendar` (`id`, `title`, `description`, `color`, `start_date`, `end_date`, `create_at`, `create_by`, `modified_at`, `modified_by`) VALUES
-(74, 'Jangan Nunggu Deadline ya :)', '', '#FF0000', '2022-12-22', '2022-12-23', '2022-12-22 08:01:46', NULL, '2022-12-22 08:04:24', NULL),
-(75, 'Pelantikan Ketua Ormawa', 'Pelantikan Birokrasi', '#0071c5', '2022-12-23', '2022-12-24', '2022-12-22 12:30:43', NULL, NULL, NULL);
+(74, 'Jangan Nunggu Deadline ya :)', '', '#FF0000', '2022-12-22', '2022-12-23', '2022-12-22 08:01:46', NULL, '2022-12-22 08:04:24', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` bigint(10) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`id`, `name`, `status`) VALUES
+(1, 'Java Basic', 1),
+(2, 'PHP Basic', 1),
+(3, 'HTML Basic', 1),
+(4, 'Kotlin', 1),
+(5, 'Spring Boot', 1),
+(6, 'Codeigniter Framework', 1);
 
 -- --------------------------------------------------------
 
@@ -129,7 +178,7 @@ CREATE TABLE `customers` (
   `total_activity` varchar(50) NOT NULL DEFAULT '''''',
   `class` varchar(50) NOT NULL DEFAULT '',
   `activity` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `customers`
@@ -169,14 +218,15 @@ CREATE TABLE `db_absensi` (
   `status_pegawai` int(1) NOT NULL,
   `keterangan_absen` varchar(100) NOT NULL,
   `maps_absen` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `db_absensi`
 --
 
 INSERT INTO `db_absensi` (`id_absen`, `kode_absen`, `nama_pegawai`, `kode_pegawai`, `tgl_absen`, `jam_masuk`, `jam_pulang`, `status_pegawai`, `keterangan_absen`, `maps_absen`) VALUES
-(3, 'absen_20221214445', 'Dedi Rosadi', '20552011053', 'Selasa, 13 Desember 2022', '10:43:10', '19:36:31', 2, 'Belajar Di Kampus', '-6.9244532, 107.6622627');
+(3, 'absen_20221214445', 'Dedi Rosadi', '20552011053', 'Selasa, 13 Desember 2022', '10:43:10', '19:36:31', 2, 'Belajar Di Kampus', '-6.9244532, 107.6622627'),
+(12, 'absen_20221286321', 'Dedi Rosadi', '973829271834', 'Rabu, 28 Desember 2022', '14:32:02', '', 2, 'Belajar Di Kampus', '-6.9617306, 107.6333816');
 
 -- --------------------------------------------------------
 
@@ -195,7 +245,7 @@ CREATE TABLE `db_rememberme` (
   `cookie_hash` varchar(255) NOT NULL,
   `expired` int(128) NOT NULL,
   `date_created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -214,7 +264,7 @@ CREATE TABLE `db_setting` (
   `absen_mulai_to` varchar(13) NOT NULL,
   `absen_pulang` varchar(13) NOT NULL,
   `maps_use` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `db_setting`
@@ -222,6 +272,75 @@ CREATE TABLE `db_setting` (
 
 INSERT INTO `db_setting` (`status_setting`, `nama_instansi`, `jumbotron_lead_set`, `nama_app_absensi`, `logo_instansi`, `timezone`, `absen_mulai`, `absen_mulai_to`, `absen_pulang`, `maps_use`) VALUES
 (1, 'TELKOM UNIVERSITY', 'Jl. Telekomunikasi. 1, Terusan Buahbatu - Bojongsoang, Telkom University, Sukapura, Kec. Dayeuhkolot, Kabupaten Bandung, Jawa', 'LMS TEL-U', 'c29a01e7627dba16a8d05b7c6eb3fcad.png', 'Asia/Jakarta', '06:30:00', '08:00:00', '16:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluation_report`
+--
+
+CREATE TABLE `evaluation_report` (
+  `id` bigint(10) NOT NULL,
+  `app_user_id` bigint(10) NOT NULL DEFAULT 0,
+  `given_answer` varchar(1000) NOT NULL,
+  `exam_date` datetime NOT NULL,
+  `course_id` bigint(10) NOT NULL DEFAULT 0,
+  `lesson_id` bigint(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lesson`
+--
+
+CREATE TABLE `lesson` (
+  `id` bigint(10) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `course_id` bigint(10) NOT NULL DEFAULT 0,
+  `status` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `lesson`
+--
+
+INSERT INTO `lesson` (`id`, `name`, `course_id`, `status`) VALUES
+(1, 'Concepts of OOPs', 1, 1),
+(2, 'Character and Boolean Data Types', 1, 1),
+(3, 'Data Structures', 1, 1),
+(4, 'Dasar Kotlin', 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questionnaire`
+--
+
+CREATE TABLE `questionnaire` (
+  `id` bigint(10) NOT NULL,
+  `course_id` bigint(10) NOT NULL DEFAULT 0,
+  `lesson_id` bigint(10) NOT NULL DEFAULT 0,
+  `right_answer` int(1) NOT NULL DEFAULT 0,
+  `title` varchar(500) DEFAULT NULL,
+  `answer_1` varchar(500) NOT NULL,
+  `answer_2` varchar(500) NOT NULL,
+  `answer_3` varchar(500) NOT NULL,
+  `answer_4` varchar(500) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `questionnaire`
+--
+
+INSERT INTO `questionnaire` (`id`, `course_id`, `lesson_id`, `right_answer`, `title`, `answer_1`, `answer_2`, `answer_3`, `answer_4`, `status`) VALUES
+(1, 1, 1, 4, 'Which of the following is not OOPS concept in Java?', 'Inheritance', 'Encapsulation', 'Polymorphism', 'Inheritance', 1),
+(2, 1, 1, 4, 'What is it called if an object has its own lifecycle and there is no owner?', 'Aggregation', 'Composition', 'Encapsulation', 'Association', 1),
+(3, 1, 1, 3, 'Which concept of Java is a way of converting real world objects in terms of class?', 'Polymorphism', 'Encapsulation', 'Abstraction', 'Inheritance', 1),
+(4, 1, 1, 1, 'Which of these values can a boolean variable contain?', 'True & False', '0 & 1', 'Any integer value', 'true', 1),
+(5, 1, 1, 1, 'Which one is a valid declaration of a boolean?', 'boolean b1 = 1;', 'boolean b1 = \"false\";', 'boolean b1 = false;', 'boolean b1 = \"true\";', 1),
+(6, 1, 1, 1, 'Which of these is used to perform all input & output operations in Java?', 'streams', 'Variables', 'classes', 'Methods', 1);
 
 -- --------------------------------------------------------
 
@@ -236,7 +355,7 @@ CREATE TABLE `tb_activity` (
   `activity_3` int(50) NOT NULL,
   `activity_4` int(50) NOT NULL,
   `activity_5` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_activity`
@@ -257,7 +376,7 @@ INSERT INTO `tb_activity` (`activity`, `activity_1`, `activity_2`, `activity_3`,
 CREATE TABLE `tb_faculty` (
   `faculty_id` varchar(20) NOT NULL,
   `nama_faculty` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_faculty`
@@ -282,7 +401,7 @@ CREATE TABLE `tb_jadwal` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=Active, 0=Block'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_jadwal`
@@ -309,7 +428,7 @@ CREATE TABLE `tb_kelas` (
   `kelas` varchar(20) NOT NULL,
   `activity` varchar(20) NOT NULL,
   `total` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_kelas`
@@ -331,7 +450,7 @@ CREATE TABLE `tb_nilai` (
   `Nama` varchar(150) DEFAULT NULL,
   `UTS` varchar(20) DEFAULT NULL,
   `UAS` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_nilai`
@@ -353,7 +472,7 @@ CREATE TABLE `tb_prodi` (
   `faculty_id` varchar(20) NOT NULL,
   `nama` varchar(25) NOT NULL,
   `kelas` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_prodi`
@@ -374,7 +493,7 @@ INSERT INTO `tb_prodi` (`program_id`, `faculty_id`, `nama`, `kelas`) VALUES
 CREATE TABLE `tb_semester` (
   `semester_id` varchar(20) NOT NULL,
   `nama` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_semester`
@@ -402,7 +521,7 @@ CREATE TABLE `tb_supplier` (
   `resource` varchar(20) DEFAULT NULL,
   `activity` varchar(100) DEFAULT NULL,
   `sra` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_supplier`
@@ -445,15 +564,15 @@ CREATE TABLE `user` (
   `qr_code_use` int(2) NOT NULL,
   `last_login` int(11) NOT NULL,
   `date_created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_pegawai`, `nama_lengkap`, `username`, `password`, `role_id`, `umur`, `image`, `qr_code_image`, `kode_pegawai`, `instansi`, `jabatan`, `npwp`, `tgl_lahir`, `tempat_lahir`, `jenis_kelamin`, `bagian_shift`, `is_active`, `qr_code_use`, `last_login`, `date_created`) VALUES
-(12, 'Dedi Rosadi', 'admin', '$2y$10$nzEbq607iLUzhvlKWSmDOOLU7r8Y0GDnWxYxA45bF9ExcP.lvRykG', 1, 22, '3a402057537e1ea49401790aaf34d1d3.jpg', 'qr_code_20552011053.png', '973829271834', 'TELKOM UNIVERSITY', '20552011053', '2017', '2001-04-04', 'Subang', 'Laki - Laki', 1, 1, 1, 1671781394, 1670857167),
-(47, 'Dedi Rosadi', 'dedi', '$2y$10$XRXong6z3evF2IG41qanPud4..ii8C/0rO/Ypr9f4MAGzS1qbNe6.', 2, 21, '24eba96bab2559d9b4fea156554f1bb1.png', 'qr_code_148296679305713.png', '148296679305713', 'TELKOM UNIVERSITY', '21552011076', '2020', '2001-04-04', 'Subang', 'Laki - Laki', 1, 1, 1, 0, 1671366109);
+(12, 'Dedi Rosadi', 'admin', '$2y$10$nzEbq607iLUzhvlKWSmDOOLU7r8Y0GDnWxYxA45bF9ExcP.lvRykG', 1, 22, '3a402057537e1ea49401790aaf34d1d3.jpg', 'qr_code_20552011053.png', '973829271834', 'TELKOM UNIVERSITY', '20552011053', '2017', '2001-04-04', 'Subang', 'Laki - Laki', 1, 1, 1, 1672211537, 1670857167),
+(47, 'Dedi Rosadi', 'dedi', '$2y$10$XRXong6z3evF2IG41qanPud4..ii8C/0rO/Ypr9f4MAGzS1qbNe6.', 2, 21, '24eba96bab2559d9b4fea156554f1bb1.png', 'qr_code_148296679305713.png', '148296679305713', 'TELKOM UNIVERSITY', '21552011076', '2020', '2001-04-04', 'Subang', 'Laki - Laki', 1, 1, 1, 1672192169, 1671366109);
 
 -- --------------------------------------------------------
 
@@ -470,7 +589,7 @@ CREATE TABLE `users` (
   `kelas` varchar(20) NOT NULL,
   `activity` varchar(20) NOT NULL,
   `total` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Indexes for dumped tables
@@ -495,9 +614,21 @@ ALTER TABLE `agenda`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `app_user`
+--
+ALTER TABLE `app_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `calendar`
 --
 ALTER TABLE `calendar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -520,6 +651,27 @@ ALTER TABLE `db_absensi`
 --
 ALTER TABLE `db_rememberme`
   ADD PRIMARY KEY (`id_session`);
+
+--
+-- Indexes for table `evaluation_report`
+--
+ALTER TABLE `evaluation_report`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lesson`
+--
+ALTER TABLE `lesson`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lesson_course_id_idx` (`course_id`);
+
+--
+-- Indexes for table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `questionnaire_course_id_idx` (`course_id`),
+  ADD KEY `questionnaire_lesson_id_idx` (`lesson_id`);
 
 --
 -- Indexes for table `tb_activity`
@@ -610,10 +762,22 @@ ALTER TABLE `admin`
   MODIFY `users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `app_user`
+--
+ALTER TABLE `app_user`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `calendar`
 --
 ALTER TABLE `calendar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `customers`
@@ -625,13 +789,31 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `db_absensi`
 --
 ALTER TABLE `db_absensi`
-  MODIFY `id_absen` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_absen` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `db_rememberme`
 --
 ALTER TABLE `db_rememberme`
   MODIFY `id_session` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `evaluation_report`
+--
+ALTER TABLE `evaluation_report`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lesson`
+--
+ALTER TABLE `lesson`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_jadwal`
